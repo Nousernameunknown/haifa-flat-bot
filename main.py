7fffbd15fa2d2ee3ebf0c9495ebd45c1decceb69
Nousernameunknown <148008426+Nousernameunknown@users.noreply.github.com>
2026-05-24 17:57:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram.ext import Application, CommandHandler
 from telegram import Update
 from telegram.ext import ContextTypes
 
-TOKEN = “8655114606:AAGB0NJCi1edimgo1KDEN5URYrWnrUU4N6o”
+TOKEN = "8655114606:AAGB0NJCi1edimgo1KDEN5URYrWnrUU4N6o"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("БОТ ЖИВ ✅")
